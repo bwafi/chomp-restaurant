8effab34c495be3639b51a6b782ed35123359944
Syahroni Bawafi <syahronibwf@gmail.com>
2023-05-22 20:45:05 +0700

    responsive contact section

--- components/home/ContactSection.js
+++ components/home/ContactSection.js
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 const ContactSection = () => {
   return (
-    <section className="bg-bg-content py-5 px-3 w-full flex items-center gap-20">
-      <div className="w-6/12">
+    <section className="bg-bg-content py-5 px-3 w-full flex lg:flex-row flex-col lg:items-center justify-center gap-20 text-center lg:text-left">
+      <div className="lg:w-6/12 w-full">
         <h1 className="font-bold text-5xl text-primary my-5">Call our store and takeaway when it suits you best.</h1>
         <p className="text-paragraf text-lg leading-normal lg:text-left text-center">
           Please feel free to call our store and arrange for a convenient time to pick up your order. We understand the
@@ -14,10 +14,10 @@ const ContactSection = () => {
           schedule.
         </p>
         <Link href="https://wa.me/6282335955903" target="_blank">
-          <Button className="my-10 w-10/12 md:w-auto bg-button-wave bg-primary text-white">ID +62 823-3595-5903</Button>
+          <Button className="my-10 bg-button-wave bg-primary text-white">ID +62 823-3595-5903</Button>
         </Link>
       </div>
-      <div className="w-5/12 ">
+      <div className="lg:w-5/12 w-full flex justify-center">
         <Image src="/img/section-contact.png" alt="food takeway" width={500} height={500} />
       </div>
     </section>
