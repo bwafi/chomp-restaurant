bc0b2424d0b59bb76597ffabdb4357268d79b8e8
Syahroni Bawafi <syahronibwf@gmail.com>
2023-05-26 14:53:36 +0700

    add company page, add hero page file

--- components/order/Hero.js
+++ components/order/Hero.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import HeroPage from '../HeroPage';
 
 const HeroOrder = () => {
   return (
-    <div className="w-full lg:h-[700px] h-[600px] flex items-center justify-center bg-alice lg:mt-0 mt-16">
-      <h1 className="lg:w-[55%] w-full md:text-6xl text-5xl font-extrabold text-heading text-center">
-        Get your food <span className="text-primary">delivered</span>, or <span className="text-primary">pick-up</span>{' '}
-        in store.
-      </h1>
-    </div>
+    <HeroPage>
+      Get your food <span className="text-primary">delivered</span>, or <span className="text-primary">pick-up</span> in
+      store.
+    </HeroPage>
   );
 };
 
