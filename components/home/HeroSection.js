d4be00db777bfd83f903ada9d1836f24d0688719
Syahroni Bawafi <syahronibwf@gmail.com>
2023-05-27 17:19:10 +0700

    try frammer motion on hero section home page

--- components/home/HeroSection.js
+++ components/home/HeroSection.js
@@ -1,12 +1,18 @@
+'use client';
 import React from 'react';
 import Button from '../ui/Button';
 import Image from 'next/image';
 import trustPilot from '../../public/logo/trustpilot-logo.svg';
+import { motion } from 'framer-motion';
 
 export default function HeroSection() {
   return (
     <section className="bg-white flex flex-col lg:flex-row w-full mt-32 mb-32 md:mb-32 items-center gap-32">
-      <div className="flex flex-col lg:items-start items-center lg:w-6/12 md:w-10/12 w-full lg:space-y-5 space-y-7">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3 }}
+        className="flex flex-col lg:items-start items-center lg:w-6/12 md:w-10/12 w-full lg:space-y-5 space-y-7">
         <h1 className="font-bold md:text-6xl text-5xl lg:text-left text-center">
           Beautiful food & takeaway, <span className="text-primary">delivered</span> to your door.
         </h1>
@@ -21,7 +27,7 @@ export default function HeroSection() {
             <span className="text-primary pl-1">4.8 out of 5</span> based on 2000+ reviews
           </p>
         </div>
-      </div>
+      </motion.div>
       <div>
         <Image
           src="/img/hero-img-section.png"
