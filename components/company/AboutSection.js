ab8da72ecb2891967f7f5400236efc244072bb63
Syahroni Bawafi <syahronibwf@gmail.com>
2023-05-31 11:06:36 +0700

    fixed about us page

--- components/company/AboutSection.js
+++ components/company/AboutSection.js
@@ -4,7 +4,14 @@ import Container from '../Container';
 const AboutSection = () => {
   return (
     <Container>
-      <AboutPage src="/img/team-img.png" className="bg-none mb-32" />
+      <AboutPage
+        link="/company"
+        button="Learn about us"
+        title="The home of fresh products"
+        paragraf="Apart from great food, we also offer a warm and comfortable atmosphere for you and your family to enjoy time together. Our friendly and professional service will make your visit an unforgettable experience"
+        src="/img/team-img.png"
+        className="bg-none mb-32"
+      />
     </Container>
   );
 };
--- components/home/AboutSection.js
+++ components/home/AboutSection.js
@@ -8,8 +8,7 @@ export default function AboutSection() {
       title="The home of fresh products"
       paragraf="
           Apart from great food, we also offer a warm and comfortable atmosphere for you and your family to enjoy time
-          together. Our friendly and professional service will make your visit an unforgettable experience.
-        s"
+          together. Our friendly and professional service will make your visit an unforgettable experience"
       src={'/img/about-img.png'}
     />
   );
