9976ad0e04fe1b469c12cc872b1b4440f579bde2
Syahroni Bawafi <syahronibwf@gmail.com>
2023-06-19 17:20:04 +0700

    ex

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { IoIosCart } from 'react-icons/io';
-import { FiMenu } from 'react-icons/fi';
-import MenuList from './ui/MenuList';
-import Cart from './Cart';
-import ButtonSquare from './ui/ButtonSquare';
-import Banner from './Banner';
-import { ProductContext } from '@/context/GlobalState';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { IoIosCart } from "react-icons/io";
+import { FiMenu } from "react-icons/fi";
+import MenuList from "./ui/MenuList";
+import Cart from "./Cart";
+import ButtonSquare from "./ui/ButtonSquare";
+import Banner from "./Banner";
+import { ProductContext } from "@/context/GlobalState";
 
 export default function Navbar() {
   const [showCart, setShowCart] = useState(false);
@@ -31,8 +31,14 @@ export default function Navbar() {
       <Banner />
       <nav className="w-full lg:top-8 top-0 shadow z-40 fixed bg-white py-3 lg:px-10 md:px-7 px-2 flex justify-between transition-all">
         <div className="">
-          <Link href={'/'}>
-            <Image src="/logo/logo.svg" width={40} height={40} alt="Logo" priority />
+          <Link href={"/"}>
+            <Image
+              src="/logo/logo.svg"
+              width={40}
+              height={40}
+              alt="Logo"
+              priority
+            />
           </Link>
         </div>
 
@@ -41,23 +47,24 @@ export default function Navbar() {
         <div className="flex space-x-3">
           <ButtonSquare
             className="bg-primary  relative bg-button-wave shadow hover:opacity-80 transition-opacity"
-            onClick={() => setShowCart(true)}>
+            onClick={() => setShowCart(true)}
+          >
             <div className="w-6 h-6 bg-white flex items-center justify-center rounded-full absolute -top-2 -right-3 text-primary text-sm shadow-md font-bold">
               {totalItems}
             </div>
             <IoIosCart className="text-2xl text-white" />
           </ButtonSquare>
           <ButtonSquare
-            className={`lg:hidden ${showMenu ? 'bg-primary' : 'bg-gray-200'}`}
-            onClick={() => setShowMenu(!showMenu)}>
-            <FiMenu className={`text-xl ${showMenu ? 'text-white' : ''}`} />
+            className={`lg:hidden ${showMenu ? "bg-primary" : "bg-gray-200"}`}
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <FiMenu className={`text-xl ${showMenu ? "text-white" : ""}`} />
           </ButtonSquare>
         </div>
       </nav>
-
       <MenuList
         className={`fixed z-30 py-10 top-0 lg:hidden bg-white flex flex-col space-y-10 w-full px-5 shadow-md transition-transform duration-200 ease-linear ${
-          showMenu ? 'top-16' : '-translate-y-full'
+          showMenu ? "top-16" : "-translate-y-full"
         }`}
       />
 
--- context/GlobalState.js
+++ context/GlobalState.js
@@ -1,6 +1,6 @@
-'use client';
-import { createContext, useContext, useEffect, useReducer } from 'react';
-import products from '../utils/products.json';
+"use client";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import products from "../utils/products.json";
 
 const initialState = {
   products: products,
@@ -9,45 +9,55 @@ const initialState = {
 
 const ProductReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_CART':
+    case "SET_CART":
       return {
         ...state,
         cart: action.payload,
       };
 
-    case 'ADD_TO_CART':
+    case "ADD_TO_CART":
       const { burgers, sides, drinks } = state.products;
       const itemAdd =
         burgers.find((prod) => prod.id === action.payload.id) ||
         sides.find((prod) => prod.id === action.payload.id) ||
         drinks.find((prod) => prod.id === action.payload.id);
-      const existingItem = state.cart.find((existing) => existing.id === itemAdd.id);
+      const existingItem = state.cart.find(
+        (existing) => existing.id === itemAdd.id
+      );
       return {
         ...state,
         cart: existingItem
-          ? state.cart.map((item) => (item.id === itemAdd.id ? { ...item, quantity: item.quantity + 1 } : item))
+          ? state.cart.map((item) =>
+              item.id === itemAdd.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            )
           : [...state.cart, { ...itemAdd, quantity: 1 }],
       };
 
-    case 'REMOVE_FROM_CART':
+    case "REMOVE_FROM_CART":
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload.id),
       };
 
-    case 'INCREASE_QUANTITY':
+    case "INCREASE_QUANTITY":
       return {
         ...state,
         cart: state.cart.map((item) =>
-          item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
         ),
       };
 
-    case 'DECREASE_QUANTITY':
+    case "DECREASE_QUANTITY":
       return {
         ...state,
         cart: state.cart.map((item) =>
-          item.id === action.payload.id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
+          item.id === action.payload.id && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
         ),
       };
 
@@ -65,7 +75,7 @@ export const GlobalProvider = ({ children }) => {
 
   function addToCart(itemId) {
     dispatch({
-      type: 'ADD_TO_CART',
+      type: "ADD_TO_CART",
       payload: {
         id: itemId,
       },
@@ -74,34 +84,34 @@ export const GlobalProvider = ({ children }) => {
 
   function removeFromCart(itemId) {
     dispatch({
-      type: 'REMOVE_FROM_CART',
+      type: "REMOVE_FROM_CART",
       payload: { id: itemId },
     });
   }
 
   function increaseQuantity(itemId) {
     dispatch({
-      type: 'INCREASE_QUANTITY',
+      type: "INCREASE_QUANTITY",
       payload: { id: itemId },
     });
   }
 
   function decreaseQuantity(itemId) {
     dispatch({
-      type: 'DECREASE_QUANTITY',
+      type: "DECREASE_QUANTITY",
       payload: { id: itemId },
     });
   }
 
   useEffect(() => {
-    const savedCart = localStorage.getItem('cart');
+    const savedCart = localStorage.getItem("cart");
     if (savedCart) {
-      dispatch({ type: 'SET_CART', payload: JSON.parse(savedCart) });
+      dispatch({ type: "SET_CART", payload: JSON.parse(savedCart) });
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(state.cart));
+    localStorage.setItem("cart", JSON.stringify(state.cart));
   }, [state.cart]);
 
   const value = {
@@ -113,5 +123,7 @@ export const GlobalProvider = ({ children }) => {
     decreaseQuantity,
   };
 
-  return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+  );
 };
